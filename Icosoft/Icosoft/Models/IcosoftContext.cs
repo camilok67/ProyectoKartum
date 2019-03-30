ac7e3ad0ab47526f7230abb59b05d442a92adc85
luisjosetorres12 <luis.torres784@gmail.com>
2019-03-29 22:12:39 -0500

    Subido

--- Icosoft/Icosoft/Models/IcosoftContext.cs
+++ Icosoft/Icosoft/Models/IcosoftContext.cs
@@ -8,7 +8,7 @@ namespace Icosoft.Models
 {
     public class IcosoftContext : DbContext
     {
-        // You can add custom code to this file. Changes will not be overwritten.
+        // You can add custom code to this file. Changes will not be overwritten
         // 
         // If you want Entity Framework to drop and regenerate your database
         // automatically whenever you change your model schema, please use data migrations.
