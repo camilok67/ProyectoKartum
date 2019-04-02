868421f6c3a91c62f8ee29e094b34c1a51c37f09
camilok67 <47872767+camilok67@users.noreply.github.com>
2019-04-01 21:21:54 -0500

    Imagen en proceso

---
+++ Icosoft/Icosoft/Class/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Icosoft.Class
+{
+    public class FileHelper
+    {
+        public static string uploadphoto(HttpPostedFileBase file, string folder)
+        {
+            var path = string.Empty;
+            var pic = string.Empty;
+
+            if (file != null)
+            {
+                pic = path.GetFileName(file.FileName);
+                path = path.Combine(HttpContext.Current.Server.MapPath(folder), pic);
+                file.SaveAs(path);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+            }
+
+            return pic;
+        }
+    }
+}
